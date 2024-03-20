e4ae50accb20ad24b3878991dc5d7f212dadff9a
Lucas M <56313048+OTalDoL2@users.noreply.github.com>
2024-03-20 20:34:13 -0300

    Update POO code

--- Atividade 03/main.dart
+++ Atividade 03/main.dart
@@ -4,25 +4,65 @@ class Carro {
   String? cor;
   String? combustivel;
   bool ligado = false;
+  double velocidade = 0;
+  var cambio = ['Neutro', '1', '2', '3', '4', '5', 'R'];
 
-  
-
-  void ligar(){
-    print('ligando carro');
-    ligado = true;
+  void ligar() {
+    if (ligado == false) {
+      print('ligando carro');
+      ligado = true;
+    }
   }
 
-  void acelerar(){
-    print('acelerando carro');
+  void desligar() {
+    if (ligado == true) {
+      print('desligando carro');
+      ligado = false;
+    }
   }
 
-  void frear(){
-    print('freando carro');
+  void acelerar() {
+    if (velocidade >= 0 && velocidade < 200) {
+      velocidade++;
+      print('Velocidade: $velocidade');
+    }
+  }
+
+  void desacelerar() {
+    if (velocidade >= 0 && velocidade < 200) {
+      velocidade--;
+      print('Velocidade: $velocidade');
+    }
+  }
+
+  void frear() {
+    if (velocidade >= 0 && velocidade < 200) {
+      velocidade -= 10;
+      if (velocidade < 1) {
+        velocidade = 0;
+      }
+      print('Velocidade: $velocidade');
+    }
   }
 }
 
-
-void main(){
+void main() {
   Carro carro1 = Carro();
-
+  carro1.marca = 'Audi';
+  carro1.modelo = 'A5';
+  carro1.cor = 'Vermelho';
+  carro1.combustivel = 'Gasolina';
+  carro1.ligar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.acelerar();
+  carro1.desacelerar();
+  carro1.frear();
 }
